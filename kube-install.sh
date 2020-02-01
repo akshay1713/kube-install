d679d31eac79a5a8973b834b94efe873f0d70047
Akshay Singh <akshaysingh.1713@gmail.com>
2020-01-31 23:29:43 -0700

    Add files via upload

---
+++ kube-install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu 
+   $(lsb_release -cs) 
+   stable"
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat << EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt-get update
+sudo apt-get install -y docker-ce kubelet kubeadm kubectl
+sudo apt-mark hold docker-ce kubelet kubeadm kubectl
+
+sudo swapoff -a
